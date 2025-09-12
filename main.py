2d8334e66a5209b1af6f9f5812be70262711a911
Sinha-Saurav <sinha69saurav@gmail.com>
2025-09-12 08:19:57 +0530

    done same changes for main.py (api)

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import pandas as pd
 app = FastAPI(title="Plant Disease Detection API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # allow requests from any website
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],   # allow GET, POST, etc.
-    allow_headers=["*"]    # allow headers like Content-Type
+    allow_methods=["*"],   
+    allow_headers=["*"]    
 )
 
 # Load Treatment Dataset
@@ -30,13 +30,13 @@ model.eval()
 
 # Image Transform
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),   # must match training input
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406],
                          [0.229, 0.224, 0.225])
 ])
 
-# Helper: Prettify Label
+
 def prettify_label(label: str) -> str:
     if "___" in label:
         plant, disease = label.split("___", 1)
@@ -44,24 +44,30 @@ def prettify_label(label: str) -> str:
         return f"{plant} - {disease}"
     return label.replace("_", " ")
 
-# Helper: Clean treatments into bullet points
 def clean_treatments(treatments: str):
-    # Split by "OR"
     parts = [t.strip() for t in treatments.split("OR")]
-    # Also split further by newlines if present
     final_parts = []
     for p in parts:
-        sub_parts = [sp.strip() for sp in p.split("\n") if sp.strip()]
+        sub_parts = [sp.strip() for sp in p.split(".") if sp.strip()]
         final_parts.extend(sub_parts)
     return final_parts
 
-# Helper: Get Disease Info  
+def bio_organic_treatments(treatments: str):
+    parts = [t.strip() for t in treatments.split(".")]
+    final_parts = []
+    for p in parts:
+        final_parts.append(p)
+    return final_parts
+
+
+  
 def get_disease_info(class_index: int):
     row = treatment_df.iloc[class_index]
     return {
         "disease": prettify_label(row["disease_Class_Name"]),
         "description": row["Disease_Description"],
-        "treatments": clean_treatments(row["Treatment_Recommendations"])
+        "treatments": clean_treatments(row["Treatment_Recommendations"]),
+        "bio_treatments": bio_organic_treatments(row["Bio_Pesticides_Fertilizers"])
     }
 
 
